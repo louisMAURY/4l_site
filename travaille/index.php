6909af7195d8d0ad6aa61becfad20bb7cc00d6e3
Louis MAURY <louis3340@gmail.com>
2019-11-27 10:48:34 +0100

    :sparkles: HTML ajouté pour une façade

---
+++ travaille/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <title>4Y Trophy</title>
+        <link src="style.css" type="stylesheet" />
+    </head>
+    <body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="#">Accueil</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+            <ul>
+                <li>Facebook</li>
+                <li>Instagram</li>
+            </ul>
+        </nav>
+        <div class="Dernier_actu">
+            <h1>Actualités</h1>
+            <img src="images/fake-article.JPG" />
+        </div>
+        <div class="GPS">
+            <h2>GPS</h2>
+            <img src="images/fake-gps.gif" />
+        </div>
+        <div id="sponsors">
+            <img src="images/renault.png" />
+        </div>
+        <div id="sponsors">
+            <img src="images/renault.png" />
+        </div>
+        <div id="sponsors">
+            <img src="images/renault.png" />
+        </div>
+        <div id="sponsors">
+            <img src="images/renault.png" />
+        </div>
+    </header>
+    </body>
+</html>
